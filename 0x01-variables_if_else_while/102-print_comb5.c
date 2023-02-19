7b81893b192c7c54e99b5666eac73f1c2b7c716b
Ibrahim Taofik <taofikibrahim6@gmail.com>
2023-02-19 11:09:55 -0800

    Task ad

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,9 +14,12 @@ int main(void)
 		{
 			putchar('0' + i / 10);
 			putchar('0' + i % 10);
+
 			putchar(' ');
+
 			putchar('0' + j / 10);
 			putchar('0' + j % 10);
+
 			if (i == 98 && j == 99)
 			{
 				break;
